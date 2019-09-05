0f6a390aa4bfaa25940723b8239c6eb951ee4000
Angelo Nieto <angelont@gmail.com>
2019-09-05 18:01:35 -0500

    Avance Angular

--- AngularTest/src/app/app.module.ts
+++ AngularTest/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/Inicio.Component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularTest/src/app/inicio/inicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
