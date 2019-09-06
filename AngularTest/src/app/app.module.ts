8e4acc52b315039de506c5a02b24401c2c16fb9d
Nieto Triana Angelo <Angelo.Nieto@carvajal.com>
2019-09-06 15:35:41 -0500

    Test Angular

--- AngularTest/src/app/app.module.ts
+++ AngularTest/src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule ,MatCheckboxModule} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/Inicio.Component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,14 @@ import { InicioComponent } from './inicio/Inicio.Component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularTest/src/app/inicio/inicio.component.ts
+++ AngularTest/src/app/inicio/inicio.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class HomeComponent implements OnInit {
+export class InicioComponent implements OnInit {
+  metas: IMeta[];
+  titulo: string = 'Diario';
   constructor() { }
-
   ngOnInit() {
+    this.metas = this.getMetas();
   }
-
+  getMetas(): IMeta[] {
+    return [{
+      id: 1,
+      title: "Terminar Sprint anterior",
+      description: "Pendiente validación de pruebas"
+    }, {
+      id: 2,
+      title: "Realizar el frontend componente home css",
+      description: "Contrucción de estilos"
+    }, {
+      id: 3,
+      title: "Realizar el frontend componente home html",
+      description: "Construcción del template"
+    }
+    ]
+  }
 }
 
+interface IMeta {
+  id: number;
+  title: string;
+  description?: string;
+}
